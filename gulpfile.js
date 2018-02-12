d96253e42e76ceb01d00464feb8ff9dfb99234ff
vinoduiexpect <vinod.uiexpect@gmail.com>
2018-02-12 17:29:29 +0530

    first session

---
+++ gulpfile.js
@@ -0,0 +1,16 @@
+var gulp = require('gulp');
+var browserSync = require('browser-sync');
+
+gulp.task('appServer' , function(){
+	browserSync.init({
+		port:9090,
+		server:{
+			baseDir:'build',
+			routes:{
+				'/bower_components':'bower_components'
+			}
+		}
+	})
+
+	gulp.watch(['build/**/*.*','src/**/*.*']).on('change', browserSync.reload);
+})
